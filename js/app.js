1d5cdc58e202e1bf91a05d6e55974c8e61ab3f82
Agustin Baiud <abaiud55@gmail.com>
2023-12-20 03:17:39 -0300

    index y contacto modificado, js agregado

---
+++ js/app.js
@@ -0,0 +1,30 @@
+ScrollReveal({
+    duration: 1000,
+    distance: '80px',
+})
+    
+ScrollReveal().reveal('.hero__heading', {distance: '0px', scale: 0.9});
+ScrollReveal().reveal('.intendencia__heading', {delay: 300,  origin: 'right'});
+ScrollReveal().reveal('.intendencia__descripcion', {delay: 300,  origin: 'left'});
+ScrollReveal().reveal('.intendencia__picture', {delay: 200, origin: 'left'});
+ScrollReveal().reveal('.destacados__heading', {delay: 100, origin: 'left'});
+ScrollReveal().reveal('.destacados__descripcion', {delay: 300, origin: 'left'});
+ScrollReveal().reveal('.destacados__enlace', {delay: 500, distance: '0px'});
+ScrollReveal().reveal('.destacados__enlace--inscripcion', {delay: 700, distance: '0px'});
+ScrollReveal().reveal('.seccion_01', {reset: true, delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.seccion_02', {reset: true, delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.seccion_03', {reset: true, delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.seccion_04', {reset: true, delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.seccion_05', {reset: true, delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.seccion_06', {reset: true, delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.canal__heading', {delay: 200, origin: 'left'});
+ScrollReveal().reveal('.canal__descripcion', {delay: 200, origin: 'right'});
+ScrollReveal().reveal('.noticias__heading', {delay: 200, origin: 'left'});
+ScrollReveal().reveal('.noticia_01', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.noticia_02', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.noticia_03', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.noticia_04', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.noticia_05', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.noticia_06', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.violencia__texto', {delay: 200, origin: 'left'});
+ScrollReveal().reveal('.violencia__contenido', {delay: 200, origin: 'bottom'});
